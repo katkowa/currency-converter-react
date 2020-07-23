bb3fbba5b0bb8d7b0577454553cc434fadff71eb
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-07-23 23:34:02 +0200

    Updates

---
+++ precache-manifest.d423bae7a2202dd65e4048f8767c4a0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c8b4b715ab32760b8e35e74c37288c",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "8d1a1d69566b283accd3",
+    "url": "/currency-converter-react/static/css/main.a6c62cb0.chunk.css"
+  },
+  {
+    "revision": "cb70966e9e6cb160b785",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1a1d69566b283accd3",
+    "url": "/currency-converter-react/static/js/main.a5f337dc.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  },
+  {
+    "revision": "0720919d7c4a739409690f900f0ffba8",
+    "url": "/currency-converter-react/static/media/background.0720919d.png"
+  }
+]);
