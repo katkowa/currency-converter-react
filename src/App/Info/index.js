24b1776d6cf9fd8f94b9872961c6008cb5e20ca4
Katarzyna Kowalska <kasia.kow94@gmail.com>
2020-08-30 02:07:18 +0200

    remove unnecessary condition

--- src/App/Info/index.js
+++ src/App/Info/index.js
@@ -4,7 +4,7 @@ import { Wrapper, Date, Line } from "./styled";
 const Info = ({ data }) => !!data && (
     <Wrapper>
         <Line>Kursy walut pobierane są z Europejskiego Banku Centralnego.</Line>
-        <Line>Aktualne na dzień: <Date>{data ? data.date : ""}</Date></Line>
+        <Line>Aktualne na dzień: <Date>{data.date}</Date></Line>
     </Wrapper>
 );
 
